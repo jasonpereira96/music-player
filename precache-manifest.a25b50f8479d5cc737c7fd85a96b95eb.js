6fc010e004087bce990b9a1a65a3dee9074ad492
jasonpereira96 <jasonpereira074@gmail.com>
2020-04-27 18:50:46 +0530

    Updates

---
+++ precache-manifest.a25b50f8479d5cc737c7fd85a96b95eb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91fc367a15fcc20d41471399b490da9",
+    "url": "/music-player/index.html"
+  },
+  {
+    "revision": "f4f13269294b06013888",
+    "url": "/music-player/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "54f78ad19a4b065f5062",
+    "url": "/music-player/static/css/main.38efbaa9.chunk.css"
+  },
+  {
+    "revision": "f4f13269294b06013888",
+    "url": "/music-player/static/js/2.249aeea3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/music-player/static/js/2.249aeea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f78ad19a4b065f5062",
+    "url": "/music-player/static/js/main.5bf4e04c.chunk.js"
+  },
+  {
+    "revision": "66bc9069cc569b161e81",
+    "url": "/music-player/static/js/runtime-main.bdb22342.js"
+  },
+  {
+    "revision": "c3f8f731e71637f8b8e75e85c801cb36",
+    "url": "/music-player/static/media/Attraction.c3f8f731.mp3"
+  },
+  {
+    "revision": "8cbda0a393b6ceaae03d7b5d6a42adb7",
+    "url": "/music-player/static/media/Bright.8cbda0a3.mp3"
+  },
+  {
+    "revision": "13d65aac708d6193489742e26ef98bf6",
+    "url": "/music-player/static/media/Castlevania.13d65aac.mp3"
+  },
+  {
+    "revision": "f4fbc5a2a0e29f095c7d6e66f7ac35ca",
+    "url": "/music-player/static/media/Dragonfly.f4fbc5a2.mp3"
+  },
+  {
+    "revision": "ceeb66af379ad9045df6f800a3b76331",
+    "url": "/music-player/static/media/Hindsight.ceeb66af.mp3"
+  },
+  {
+    "revision": "b74d8df994bc67433aeee8f989619018",
+    "url": "/music-player/static/media/Lazy_Boy_Blues.b74d8df9.mp3"
+  },
+  {
+    "revision": "af0f9b927c44a04a35baff99a8f46fd8",
+    "url": "/music-player/static/media/Sunshine_on_Sand.af0f9b92.mp3"
+  },
+  {
+    "revision": "1298b2766b1c4681be9ccc2680b0ac18",
+    "url": "/music-player/static/media/Whirlpool.1298b276.mp3"
+  },
+  {
+    "revision": "4cc10c1b5278bbc0817fe9dd4b8ca26d",
+    "url": "/music-player/static/media/jc.4cc10c1b.jpg"
+  }
+]);
