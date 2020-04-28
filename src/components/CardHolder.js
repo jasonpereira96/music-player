fdc322b1af94166dd4ef3c5ed6bc305d646b5c60
jasonpereira96 <jasonpereira074@gmail.com>
2020-04-28 17:31:59 +0530

    resolving duplicate keys issue for now

--- src/components/CardHolder.js
+++ src/components/CardHolder.js
@@ -12,12 +12,12 @@ function CardHolder(props) {
     let count = 0;
     while (number >= 5) {
         cardDecks = cardDecks.concat(
-            <CardDeck style={{ padding: 20 }}>
-                <SongCard key={songs[count + 0].id} artist={songs[count + 0].artist} title={songs[count + 0].title} songId={songs[count + 0].id} image={songs[count + 0].image} />
-                <SongCard key={songs[count + 1].id} artist={songs[count + 1].artist} title={songs[count + 1].title} songId={songs[count + 1].id} image={songs[count + 1].image} />
-                <SongCard key={songs[count + 2].id} artist={songs[count + 2].artist} title={songs[count + 2].title} songId={songs[count + 2].id} image={songs[count + 2].image} />
-                <SongCard key={songs[count + 3].id} artist={songs[count + 3].artist} title={songs[count + 3].title} songId={songs[count + 3].id} image={songs[count + 3].image} />
-                <SongCard key={songs[count + 4].id} artist={songs[count + 4].artist} title={songs[count + 4].title} songId={songs[count + 4].id} image={songs[count + 4].image} />
+            <CardDeck style={{ padding: 20 }} key='4'>
+                <SongCard artist={songs[count + 0].artist} title={songs[count + 0].title} songId={songs[count + 0].id} image={songs[count + 0].image} />
+                <SongCard artist={songs[count + 1].artist} title={songs[count + 1].title} songId={songs[count + 1].id} image={songs[count + 1].image} />
+                <SongCard artist={songs[count + 2].artist} title={songs[count + 2].title} songId={songs[count + 2].id} image={songs[count + 2].image} />
+                <SongCard artist={songs[count + 3].artist} title={songs[count + 3].title} songId={songs[count + 3].id} image={songs[count + 3].image} />
+                <SongCard artist={songs[count + 4].artist} title={songs[count + 4].title} songId={songs[count + 4].id} image={songs[count + 4].image} />
             </CardDeck>);
         number = number - 5;
         count = count + 5;
@@ -28,9 +28,9 @@ function CardHolder(props) {
         remainingCards.push(<SongCard key={songs[i].id} title={songs[i].title} artist={songs[i].artist} songId={songs[i].id} image={songs[i].image} />)
     }
     for (var i = 1; i <= numberOfEmptyCards; i++) {
-        remainingCards.push(<EmptyCard key={songs.length + i} />);
+        remainingCards.push(<EmptyCard />);
     }
-    cardDecks = cardDecks.concat(<CardDeck style={{ padding: 20 }}>{remainingCards}</CardDeck>);
+    cardDecks = cardDecks.concat(<CardDeck style={{ padding: 20 }} key='5'>{remainingCards}</CardDeck>);
     return (
         <Container fluid className="card-holder">
             {cardDecks}
