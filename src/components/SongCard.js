2d6c756fde01f3f02d68145b29158483d2dce93f
jasonpereira96 <jasonpereira074@gmail.com>
2020-08-06 12:37:30 +0530

    cursor on hover changes and click issue fix

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -4,8 +4,8 @@ import { play } from './../actions/actions'
 import { connect } from 'react-redux'
 
 function SongCard(props) {
-    return (<Card style={{ width: '18rem' }} className='song-card'>
-        <Card.Img variant="top" src={props.image} onClick={function () { props.onClick(props.songId) }} fluid='true'/>
+    return (<Card style={{ width: '18rem' }} className='song-card' onClick={function () { props.onClick(props.songId) }}>
+        <Card.Img variant="top" src={props.image} fluid='true'/>
         <Card.Body>
             <Card.Title>{props.title}</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">{props.artist}</Card.Subtitle>
--- src/utils/fetchSongs.js
+++ src/utils/fetchSongs.js
@@ -2,14 +2,15 @@ const firebase = require('firebase');
 
 function fetchSongs() {
     var app = firebase.initializeApp({
-        apiKey: "",
-        authDomain: "",
-        databaseURL: "",
-        projectId: "",
-        storageBucket: "",
-        messagingSenderId: "",
-        appId: ""
+        apiKey: "AIzaSyB7pv_uxH1W4KXA2a31Gzw7ZHeeSVrlSgs",
+        authDomain: "music-player-c5407.firebaseapp.com",
+        databaseURL: "https://music-player-c5407.firebaseio.com",
+        projectId: "music-player-c5407",
+        storageBucket: "music-player-c5407.appspot.com",
+        messagingSenderId: "772178190113",
+        appId: "1:772178190113:web:1b0120b46b831463c92860"
     })
+
     var database = firebase.database(app);
     var ref = database.ref();
 
